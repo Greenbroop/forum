76c4c10c345c0ce45bcb449b8e451094d0a19f13
hai Dang <pleiku.6565@gmail.com>
2026-05-31 18:30:30 +0700

    message

--- src/main/java/com/iu/forum/repository/MessageRepository.java
+++ src/main/java/com/iu/forum/repository/MessageRepository.java
@@ -1,5 +1,19 @@
 package com.iu.forum.repository;
 
-public class MessageRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iu.forum.model.Message;
+import com.iu.forum.model.Thread;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    
+    // Lấy danh sách toàn bộ tin nhắn thuộc về một Thread cụ thể
+    List<Message> findByThread(Thread thread);
+    
+    // Tự động xóa tất cả các tin nhắn thuộc một Thread (Dùng khi xóa Thread)
+    void deleteByThread(Thread thread);
 }
