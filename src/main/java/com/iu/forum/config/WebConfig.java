7e0764d052aae00e886315f48538862c90d7d35c
Greenbroop <bongden0329@gmail.com>
2026-05-31 18:21:16 +0700

    security +

--- src/main/java/com/iu/forum/config/WebConfig.java
+++ src/main/java/com/iu/forum/config/WebConfig.java
@@ -1,5 +1,21 @@
 package com.iu.forum.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Cấu hình link ảo /uploads/** trỏ thẳng vào folder uploads thật ngoài ổ đĩa máy tính
+        String uploadDir = System.getProperty("user.dir") + "/uploads/";
+        Path uploadPath = Paths.get(uploadDir);
+        
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/" + uploadPath.toAbsolutePath().toString() + "/");
+    }
 }
