47ab92beb483aedcd4df5d119b476b6a398ba503
hai Dang <pleiku.6565@gmail.com>
2026-05-31 18:31:48 +0700

    user

--- src/main/java/com/iu/forum/repository/UserRepository.java
+++ src/main/java/com/iu/forum/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.iu.forum.repository;
 
-import com.iu.forum.model.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import com.iu.forum.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
