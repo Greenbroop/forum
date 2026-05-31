ee66d7df9c71f97009f1d12cc9b4990a7148d8f9
hai Dang <pleiku.6565@gmail.com>
2026-05-31 18:31:04 +0700

    thread

--- src/main/java/com/iu/forum/repository/ThreadRepository.java
+++ src/main/java/com/iu/forum/repository/ThreadRepository.java
@@ -1,5 +1,21 @@
 package com.iu.forum.repository;
 
-public class ThreadRepository {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iu.forum.model.Thread;
+
+@Repository
+public interface ThreadRepository extends JpaRepository<Thread, Long> {
+    
+    // Lấy tất cả bài chưa bị xóa (Sắp xếp theo ý muốn)
+    List<Thread> findByDeletedFalse(Sort sort);
+    List<Thread> findByCreatedAtBeforeAndDeletedFalse(LocalDateTime date);
+
+    // Tìm kiếm bằng từ khóa đối với các bài chưa bị xóa
+    List<Thread> findByTitleContainingIgnoreCaseAndDeletedFalse(String keyword);
 }
