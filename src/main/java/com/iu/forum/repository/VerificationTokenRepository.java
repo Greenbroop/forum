9c42a4441bc260406bb2f4ca274ba70f4a6de502
hai Dang <pleiku.6565@gmail.com>
2026-05-31 18:32:26 +0700

    veridfication token

--- src/main/java/com/iu/forum/repository/VerificationTokenRepository.java
+++ src/main/java/com/iu/forum/repository/VerificationTokenRepository.java
@@ -1,5 +1,13 @@
 package com.iu.forum.repository;
 
-public class VerificationTokenRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.iu.forum.model.VerificationToken;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
 }
